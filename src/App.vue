228eae22abb976046207fb99daa467e4bcb67d6d
Jandoria <alejandro@cryptobirds.com>
2023-11-20 03:52:43 +0100

    Improv. UI/UX

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,46 @@
 <template>
-  <h1 class="text-5xl font-bold mb-5">Simple Playlist Exporter</h1>
-
+  <h1 class="text-5xl font-bold mb-16">Simple Playlist Exporter</h1>
+  <!-- 1. Login with Spotify -->
   <button v-if="!userProfile" @click="login">Login with Spotify</button>
   <section v-else id="profile">
-    <div class="flex items-center gap-4 p-4">
-      <img :src="userProfile.images[0].url" width="64" alt="Spotify profile picture" class="rounded-full" />
-      <div class="flex flex-col">
-        <strong class="text-slate-900 font-semibold dark:text-slate-200">{{ userProfile.display_name }}</strong>
-        <a
-          :href="userProfile.external_urls.spotify"
-          target="_blank"
-          class="text-slate-500 dark:text-slate-400 flex items-center"
-        >
-          View profile <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 ml-1">
-            <path fill-rule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clip-rule="evenodd" />
-            <path fill-rule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clip-rule="evenodd" />
-          </svg>
-        </a>
+    <div class="flex items-center justify-between">
+      <div class="flex items-center gap-4 p-4">
+        <img :src="userProfile.images[0].url" width="64" alt="Spotify profile picture" class="rounded-full" />
+        <div class="flex flex-col">
+          <strong class="text-slate-900 font-semibold dark:text-slate-200 flex">{{ userProfile.display_name }}</strong>
+          <a
+            :href="userProfile.external_urls.spotify"
+            target="_blank"
+            class="text-slate-500 dark:text-slate-400 flex items-center"
+          >
+            View profile <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 ml-1">
+              <path fill-rule="evenodd" d="M4.25 5.5a.75.75 0 00-.75.75v8.5c0 .414.336.75.75.75h8.5a.75.75 0 00.75-.75v-4a.75.75 0 011.5 0v4A2.25 2.25 0 0112.75 17h-8.5A2.25 2.25 0 012 14.75v-8.5A2.25 2.25 0 014.25 4h5a.75.75 0 010 1.5h-5z" clip-rule="evenodd" />
+              <path fill-rule="evenodd" d="M6.194 12.753a.75.75 0 001.06.053L16.5 4.44v2.81a.75.75 0 001.5 0v-4.5a.75.75 0 00-.75-.75h-4.5a.75.75 0 000 1.5h2.553l-9.056 8.194a.75.75 0 00-.053 1.06z" clip-rule="evenodd" />
+            </svg>
+          </a>
+        </div>
       </div>
+      <button id="close" @click="logout">Logout</button>
     </div>
-    <button id="get-playlists" class="mr-4" @click="getPlaylists">Get my playlists</button>
-    <button id="close" @click="logout">Close session</button>
-    <div class="max-w-md border-t border-slate-500 my-6" />
-    <PlaylistCard
-      v-for="playlist in playlists"
-      :key="playlist.id"
-      :image="playlist.images[0].url"
-      :name="playlist.name"
-      :description="playlist.description"
-      :external_url="playlist.external_urls.spotify"
-    />
+    <button v-if="!playlists.length" class="my-4" @click="getPlaylists">Get my playlists</button>
+    <div v-if="playlists.length > 0" class="max-w-md border-y border-slate-500 divide-y divide-slate-500 divide-dashed my-6">
+      <PlaylistCard
+        v-for="playlist in playlists"
+        :key="playlist.id"
+        :image="playlist.images[0].url"
+        :name="playlist.name"
+        :description="playlist.description"
+        :external_url="playlist.external_urls.spotify"
+      />
+    </div>
   </section>
+  <!-- 2. or paste link to public playlist -->
+  <div class="my-6">——— or ———</div>
+  <p>Paste link to public playlist</p>
+  <input type="text" />
+  <!-- TODO  v-model="playlistUrl"-->
+  <!-- 3. Export -->
+  <!-- TODO -->
 </template>
 
 <script lang="ts">
@@ -110,6 +120,7 @@ export default defineComponent({
       if (storedAccessToken) {
         const profile = await fetchProfile(storedAccessToken);
         if (profile) state.userProfile = profile;
+        await getPlaylists();
       } else {
         const params = new URLSearchParams(window.location.search);
         const code = params.get("code");
